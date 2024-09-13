fc5ffeb00923066775a2480b20d90562b88ae205
Kimani Nyutu <capalot4802@gmail.com>
2024-09-13 09:02:42 +0300

    feat: dynamically show form fields based on type selection

--- app/admin/dashboard/suppliers/add/page.jsx
+++ app/admin/dashboard/suppliers/add/page.jsx
@@ -1,21 +1,23 @@
 "use client";
 import { useState } from "react";
 import styles from "../../../../student/ui/dashboard/students/addStudent/addStudent.module.css";
-import {config} from "/config";
+import { config } from "/config";
 
 const SuppliersAddPage = () => {
-
-
   const [formData, setFormData] = useState({
-    firstName: "",
+    suppliers: "",
     itemDescription: "",
     amountClaimed: "",
-    claimAnt: "",
-    dateClaimed:"",
-    idNo: "",
+    approver: "",
+    DateTakenToApprover: "",
+    DateTakenToFinance: "",
+    Type: "",
+    ClaimNumber: "",
     pvNo: "",
-    
+    Accounted: "",
+    DateAccounted: "",
   });
+
   const [successMessage, setSuccessMessage] = useState("");
 
   const handleChange = (e) => {
@@ -33,22 +35,22 @@ const SuppliersAddPage = () => {
         body: JSON.stringify(formData),
       });
       if (response.ok) {
-        // Handle successful submission
         console.log("Staff added successfully");
         setSuccessMessage("Staff added successfully!");
-        // Clear the form data
         setFormData({
           suppliers: "",
           itemDescription: "",
           amountClaimed: "",
-          claimAnt: "",
-          dateClaimed:"",
-          idNo: "",
-          phone: "",
-         
+          approver: "",
+          DateTakenToApprover: "",
+          DateTakenToFinance: "",
+          Type: "",
+          ClaimNumber: "",
+          pvNo: "",
+          Accounted: "",
+          DateAccounted: "",
         });
       } else {
-        // Handle submission error
         console.error("Failed to add Staff");
       }
     } catch (error) {
@@ -56,62 +58,154 @@ const SuppliersAddPage = () => {
     }
   };
 
+  const renderFields = () => {
+    switch (formData.Type) {
+      case "Claim":
+        return (
+            <>
+              <input
+                  type="text"
+                  placeholder="pvNo"
+                  name="pvNo"
+                  value={formData.pvNo}
+                  onChange={handleChange}
+                  required
+              />
+              <input
+                  type="text"
+                  placeholder="Claimed Number"
+                  name="ClaimNumber"
+                  value={formData.ClaimNumber}
+                  onChange={handleChange}
+                  required
+              />
+            </>
+        );
+      case "Petty Cash":
+        return (
+            <input
+                type="text"
+                placeholder="pvNo"
+                name="pvNo"
+                value={formData.pvNo}
+                onChange={handleChange}
+                required
+            />
+        );
+      case "Imprest":
+        return (
+            <>
+              <input
+                  type="text"
+                  placeholder="pcNo"
+                  name="pcNo"
+                  value={formData.pcNo}
+                  onChange={handleChange}
+                  required
+              />
+              <select
+                  name="Accounted"
+                  value={formData.Accounted}
+                  onChange={handleChange}
+                  required
+                  className={styles.select}
+              >
+                <option value="">Select Accounted</option>
+                <option value="Yes">Yes</option>
+                <option value="No">No</option>
+              </select>
+              <label className={styles.label}>
+                Date Accounted (YYYY-MM-DD HH:MM)
+              </label>
+              <input
+                  type="datetime-local"
+                  name="DateAccounted"
+                  value={formData.DateAccounted}
+                  onChange={handleChange}
+                  required
+              />
+            </>
+        );
+      default:
+        return null;
+    }
+  };
+
   return (
-    <div className={styles.container}>
-     {successMessage && <p className={styles.successMessage}>{successMessage}</p>}
-      <form onSubmit={handleSubmit} className={styles.form}>
-        <input
-          type="text"
-          placeholder="Suppliers"
-          name="suppliers"
-          value={formData.suppliers}
-          onChange={handleChange}
-          required
-        />
-        <input
-          type="text"
-          placeholder="Description"
-          name="itemDescription"
-          value={formData.itemDescription}
-          onChange={handleChange}
-          required
-        />
-        <input
-          type="text"
-          placeholder="Amount Claimed"
-          name="amountClaimed"
-          value={formData.amountClaimed}
-          onChange={handleChange}
-          required
-        />
-        <input
-          type="text"
-          placeholder="Claimed"
-          name="claimAnt"
-          value={formData.claimAnt}
-          onChange={handleChange}
-          required
-        />
-           <input
-          type="text"
-          placeholder="Date Claimed"
-          name="dateClaimed"
-          value={formData.dateClaimed}
-          onChange={handleChange}
-          required
-        />
-        <input
-          type="text"
-          placeholder="pvNo"
-          name="pvNo"
-          value={formData.pvNo}
-          onChange={handleChange}
-        />
-   
-       
-        <button type="submit">Submit</button>
-      </form>
-    </div>
+      <div className={styles.container}>
+        {successMessage && <p className={styles.successMessage}>{successMessage}</p>}
+        <form onSubmit={handleSubmit} className={styles.form}>
+          <input
+              type="text"
+              placeholder="Suppliers"
+              name="suppliers"
+              value={formData.suppliers}
+              onChange={handleChange}
+              required
+          />
+          <input
+              type="text"
+              placeholder="Description"
+              name="itemDescription"
+              value={formData.itemDescription}
+              onChange={handleChange}
+              required
+          />
+          <input
+              type="text"
+              placeholder="Amount Claimed"
+              name="amountClaimed"
+              value={formData.amountClaimed}
+              onChange={handleChange}
+              required
+          />
+          <input
+              type="text"
+              placeholder="Approver"
+              name="approver"
+              value={formData.approver}
+              onChange={handleChange}
+              required
+          />
+          <label className={styles.label}>
+            Date Taken To Approver (YYYY-MM-DD HH:MM)
+          </label>
+          <input
+              type="datetime-local"
+              name="DateTakenToApprover"
+              value={formData.DateTakenToApprover}
+              onChange={handleChange}
+              required
+          />
+          <label className={styles.label}>
+            Date Taken To Finance (YYYY-MM-DD HH:MM)
+          </label>
+          <input
+              type="datetime-local"
+              name="DateTakenToFinance"
+              value={formData.DateTakenToFinance}
+              onChange={handleChange}
+              required
+          />
+          <select
+              name="Type"
+              value={formData.Type}
+              onChange={handleChange}
+              required
+              className={styles.select}
+          >
+            <option value="">Select Type</option>
+            <option value="Claim">Claim</option>
+            <option value="Imprest">Imprest</option>
+            <option value="Petty Cash">Petty Cash</option>
+          </select>
+
+          {/* Conditional Fields Based on Type Selection */}
+          {renderFields()}
+
+          <button type="submit">Submit</button>
+        </form>
+      </div>
   );
 };
 
