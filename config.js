ef2e39d7c1cd12d6463cf00ab284b48125e18c64
Kimani Nyutu <capalot4802@gmail.com>
2024-09-16 08:44:00 +0300

    Finalized Suppliers Page

--- config.js
+++ config.js
@@ -2,7 +2,8 @@
 require('dotenv').config();
 
 export const config = {
-  baseURL: process.env.BASE_URL || 'http://localhost:10600',
+  // baseURL: process.env.BASE_URL || 'http://localhost:10600',
+  baseURL: process.env.BASE_URL || 'https://backend-1-gene.onrender.com',
 
     // baseURL: process.env.BASE_URL || 'https://vmlabackend.dkut.ac.ke',
   };
